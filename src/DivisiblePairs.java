df45c392075bfaddff482121114d95081cc4db7d
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-16 09:32:16 +0530

    updated

--- src/DivisiblePairs.java
+++ src/DivisiblePairs.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
 public class DivisiblePairs {
     
         // TODO: Read n and k
         // TODO: Read array elements
         // TODO: Count pairs (i, j) such that i < j and (arr[i] + arr[j]) % k == 0
         // TODO: Print the count
-    
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int k = sc.nextInt();
+            int count=0;
+            int arr[] = new int[n];
+
+            for(int i = 0;i<n;i++)
+            {
+                arr[i] = sc.nextInt();
+            }
+            for(int i=0;i<n;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                        if(arr[i]<arr[j])
+                        {
+                                if((arr[i]+arr[j])%k==0)
+                                {
+                                        count++;
+                                }
+                        }
+                }
+            }System.out.println(count);
+        }
 }
